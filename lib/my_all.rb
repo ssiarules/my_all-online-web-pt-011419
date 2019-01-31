125dc47777e80ffb415fa2e88a4c8c0ecb031961
Johnson Joseph <jhonsonj88@gmail.com>
2019-01-31 17:14:18 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,23 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0 
+  block_return_values = []
+  while i < collection.length 
+  block_return_values << 
+  yield(collection[i])
+  i = i + 1
+  #binding.pry 
 end
+
+my_all?([1,2,3]){|i| i < 2} 
+
+if block_return_values    
+  (false)
+      false
+    else
+      TRUE
+    end 
+  end 
+  
+  
